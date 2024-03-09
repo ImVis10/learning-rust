bc231e27e0bae8499dfa1f78c499274217d57636
Venkata Viswanath Vankadara <viswanathvankadara@gmail.com>
2024-03-09 12:07:47 -0600

    refactor: common methods for duplicated code

--- calculator/calculator.rs
+++ calculator/calculator.rs
@@ -1,51 +1,9 @@
 use std::io;
 
 fn main() {
-    println!("enter 1st number:");
-
-    // rust takes input as a string 
-    let mut input1 = String::new();
-
-    // read the line into the string
-    io::stdin()
-        .read_line(&mut input1)
-        .expect("failed to read line");
-
-    // parse the string as an integer
-    let num1: i32 = match input1.trim().parse() {
-        Ok(num1) => num1,
-        Err(_) => {
-            println!("PLEASE ENTER A VALID INTEGER");
-            return;
-        }
-    };
-
-    println!("enter 2nd number:");
-
-    let mut input2 = String::new();
-
-    io::stdin()
-        .read_line(&mut input2)
-        .expect("failed to read line");
-
-    let num2: i32 = match input2.trim().parse() {
-        Ok(num2) => num2,
-        Err(_) => {
-            println!("PLEASE ENTER A VALID INTEGER");
-            return;
-        }
-    };
-
-    println!("enter operator(+, -, *, /):");
-
-    let mut input3 = String::new();
-
-    io::stdin()
-        .read_line(&mut input3)
-        .expect("failed to read line"); 
-
-    // trim the string and take the first character
-    let op = input3.trim().chars().next().expect("no operator entered");
+    let num1 = read_number("enter 1st number:");
+    let num2 = read_number("enter 2nd number:");
+    let op = read_operator("enter operator:");
 
     let result = match op {
         '+' => num1 + num2,
@@ -58,11 +16,55 @@ fn main() {
             }
             num1 / num2
         },
-        _ => {
+        _ => { // defensive programming
             println!("unsupported operator");
             return;
         }
     };
 
     println!("Result: {}", result);
+}
+
+fn read_number(prompt: &str) -> i32 {
+    loop {
+        println!("{}", prompt);
+
+        // rust takes input as a string 
+        let mut input = String::new();
+
+        // read the line into the string
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read line");
+
+        // parse the string as an integer
+        match input.trim().parse::<i32>() {
+            Ok(num) => return num,
+            Err(_) => {
+                println!("PLEASE ENTER A VALID INTEGER");
+                continue;
+            }
+        };
+    }
+}
+
+fn read_operator(prompt: &str) -> char {
+    loop {
+        println!("{}", prompt);
+
+        let mut op = String::new();
+
+        io::stdin()
+            .read_line(&mut op)
+            .expect("failed to read line"); 
+
+        if let Some(first_char) = op.trim().chars().next() {
+            match first_char {
+                '+' | '-' | '*' | '/' => return first_char,
+                _ => println!("unsupported operator. please enter one of +, -, *, /."),
+            }
+        } else {
+            println!("no operator entered. please enter one of +, -, *, /");
+        }
+    }
 }
